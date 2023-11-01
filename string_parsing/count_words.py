2f414da7fa7c0a5e9ff795ad8f0daf3690ae6ec9
Closdlgdo <cidstp031921@gmail.com>
2023-11-01 14:14:55 -0700

    Continuing my DSA goal of practicing for interviews. Practicing string parsing. We wrote a function that counts the number of words in a sentence.

--- string_parsing/count_words.py
+++ string_parsing/count_words.py
@@ -1,1 +1,13 @@
 # Write a function that counts the number of words in a sentence.
+def count_words(sentence):
+    return len(sentence.split())
+
+
+print(count_words("How many words are in this sentence?"))
+
+# Line 3: return len(sentence.split())
+# This line contains the logic to count the words. Here's what it does:
+# sentence.split() splits the input sentence into a list of words. By default, it splits on spaces, so it separates the
+# words.
+# len(...) gets the length of the list of words, which is effectively the number of words in the sentence.
+# So, len(sentence.split()) gives you the count of words.
