db59c1b2ab244dde3cbe9b0f22a56448bdae7f75
Closdlgdo <cidstp031921@gmail.com>
2023-11-03 13:55:06 -0700

    We will learn about NumPy's. Currently just notted a bit about what NumPys.

---
+++ NumPy/practice_01.py
@@ -0,0 +1,5 @@
+# What is NumPy? NumPy is like a special tool for people who work with numbers a lot, especially in programming.
+# It makes it much easier to do math and work with big sets of numbers. Imagine if you had to do a lot of calculations
+# with a bunch of numbers, like adding, subtracting, or even more complicated stuff. NumPy helps you do all of that
+# faster and more efficiently. It's like having a powerful calculator that understands how to handle lots of numbers all
+# at once. This can be really handy for tasks like data analysis, scientific computations, and more!
