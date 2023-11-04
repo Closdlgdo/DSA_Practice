e1ae7166102b13596c41d4a4bc551463850a3625
Closdlgdo <cidstp031921@gmail.com>
2023-11-03 22:43:40 -0700

    We will learn about NumPy's. We started how to access/change elements of a numpy array.

--- NumPy/practice_01.py
+++ NumPy/practice_01.py
@@ -3,3 +3,38 @@
 # with a bunch of numbers, like adding, subtracting, or even more complicated stuff. NumPy helps you do all of that
 # faster and more efficiently. It's like having a powerful calculator that understands how to handle lots of numbers all
 # at once. This can be really handy for tasks like data analysis, scientific computations, and more!
+# It is faster to read fewer bytes of memory. Also, there is no type checking when iterating through objects.
+# NumPy uses contiguous memory.
+# How to access/change specific elements of a numpy array:
+import numpy as np
+
+a = np.array([["carlos", 2, 3, 4, 5, 6, "joel"], ["joel", 6, 7, 8, 9, 10, "carlos"], ["Lucas", 62, 57, 28, 39, 130, "Matias"]])
+print(a)
+
+# Get a specific element [row, col]
+print(a[1, 0])
+
+# Get a specific row
+print(a[0, :])
+
+# Get a specific column
+print(a[:, 2])
+
+# Get fancy [startindex:endindex:stepsize]
+print(a[0, 1:-1:2])
+
+# Change a specific element
+a[1, 0] = "Theano"
+print(a)
+
+# Change a specific row
+a[1, :] = "Theano"
+print(a)
+
+# Change a specific column
+a[:, 2] = "New Column"
+print(a)
+
+# Get specific element
+var = a[0, 2]
+print(var)
